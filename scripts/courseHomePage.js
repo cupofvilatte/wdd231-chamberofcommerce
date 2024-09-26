3947c8ff3efb76ef7b82f646074b7bb68d8b583a
Vilate <viknapp22@gmail.com>
2024-09-26 13:09:01 -0600

    changed from remaining to total credit view

--- scripts/courseHomePage.js
+++ scripts/courseHomePage.js
@@ -51,17 +51,28 @@ const categoryButtons = document.querySelectorAll('#course-categories button');
     });
 });
 
-const creditsRemaining = document.getElementById('credits-remaining');
-function calculateRemainingCredits() {
+const totalCredits = document.getElementById('total-credits');
+function calculateTotalCredits() {
     const totalCredits = courses.reduce((sum, course) => sum + course.credits, 0);
-    const completedCredits = courses.reduce((sum, course) => {
-        return course.completed ? sum + course.credits : sum;
-    }, 0);
-
-    const remainingCredits = totalCredits - completedCredits;
-    return remainingCredits;
+    return totalCredits;
 }
 
+//display total credits
+const creditsInTotal = calculateTotalCredits();
+totalCredits.textContent = `Total Credits: ${creditsInTotal}`;
+
+// remaining credits
+// const creditsRemaining = document.getElementById('credits-remaining');
+// function calculateRemainingCredits() {
+//     const totalCredits = courses.reduce((sum, course) => sum + course.credits, 0);
+//     const completedCredits = courses.reduce((sum, course) => {
+//         return course.completed ? sum + course.credits : sum;
+//     }, 0);
+
+//     const remainingCredits = totalCredits - completedCredits;
+//     return remainingCredits;
+// }
+
 // Display remaining credits
-const remainingCredits = calculateRemainingCredits();
-creditsRemaining.textContent = `Remaining Credits: ${remainingCredits}`;
+// const remainingCredits = calculateRemainingCredits();
+// creditsRemaining.textContent = `Remaining Credits: ${remainingCredits}`;
